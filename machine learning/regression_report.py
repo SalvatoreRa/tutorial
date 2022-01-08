2fe3ff27d8b186c32f27bd9771fb3fc407adc88d
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2022-01-08 18:29:09 +0100

    Update regression_report.py

--- machine learning/regression_report.py
+++ machine learning/regression_report.py
@@ -12,6 +12,60 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+def regression_report(test_set = None, target = None, predictions = None ):
+  """
+  Return the calculation of different regression metric
+  INPUT
+  test_set = test dataset
+  target = the target variable as np.array or a list
+  prediction = the prediction of the model, np.array or a list
+  OUTPUT
+  print the dataframe as a table
+  return a pandas data.frame
+
+  EXAMPLE USAGE
+  import xgboost
+  from xgboost import XGBRegressor
+  from sklearn.model_selection import train_test_split
+  import pandas as pd
+  #boston dataset
+  data_dir = "https://raw.githubusercontent.com/SalvatoreRa/tutorial/main/datasets/Boston.csv"
+  df = pd.read_csv(data_dir)
+  #separing the input features from the target variable
+  y = df["medv"]
+  X = df.drop(["medv"], axis=1)
+  #splitting the dataset
+  X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = 0.2, random_state = 42) 
+  # generate an xgboost regression model
+  model = XGBRegressor()
+  #fit the model
+  model.fit(X_train, y_train)
+  #predict for the test dataset
+  pred = model.predict(X_test)
+  _ = regression_report(test_set = X_test, target = y_test, predictions = pred )
+
+  """
+  df = pd.DataFrame(columns = ["Metric", "Acronym", "Value"])
+  metric_list = ["Mean Square Error", "Root Mean Square Error", "Max Error", "Mean Absolute Error", "Mean squared logarithmic error",
+                 "Mean Absolute Percentage Error", "R squared", "adjusted R squared", "Normalized RMSE (range)", "Normalized RMSE (quartile)"]
+  acron_list  = ["MAE", "RMSE", "max_err", "MAE", "MSLE", "MAPE", "R2", "adj_R2", "NRMSE", "NRMSE"]
+  df["Metric"], df["Acronym"]  = metric_list, acron_list
+  df.iloc[0,2] = MSE(target, predictions)
+  df.iloc[1,2] = RMSE(target, predictions)
+  df.iloc[2,2] = Max_error(target, predictions)
+  df.iloc[3,2] = MAE(target, predictions)
+  df.iloc[4,2] = MSLE(target, predictions)
+  df.iloc[5,2] = MAPE(target, predictions)
+  df.iloc[6,2] = R_squared(target, predictions)
+  df.iloc[7,2] = adj_R_squared(target, predictions,  n = len(target), k = test_set.shape[1] )
+  df.iloc[8,2] = NRMSE(target, predictions)
+  df.iloc[9,2] = NRMSE(target, predictions, norm_method = "quartile")
+  df1 = df.copy()
+  df1.value = df1.Value.astype("float32").round(decimals = 3)
+  print(df1)
+  return df
+
+
 def MSE(y = None, y_hat = None, avg = True ):
     """ Mean Square Error """
     y = np.array(y)
@@ -128,108 +182,4 @@ def Max_error(y = None, y_hat = None):
     #calculate the maximum
     return np.max(err)
 
-def regression_report(test_set = None, target = None, predictions = None ):
-  """
-  Return the calculation of different regression metric
-  INPUT
-  test_set = test dataset
-  target = the target variable as np.array or a list
-  prediction = the prediction of the model, np.array or a list
-  OUTPUT
-  print the dataframe as a table
-  return a pandas data.frame
-
-  EXAMPLE USAGE
-  import xgboost
-  from xgboost import XGBRegressor
-  from sklearn.model_selection import train_test_split
-  import pandas as pd
-  #boston dataset
-  data_dir = "https://raw.githubusercontent.com/SalvatoreRa/tutorial/main/datasets/Boston.csv"
-  df = pd.read_csv(data_dir)
-  #separing the input features from the target variable
-  y = df["medv"]
-  X = df.drop(["medv"], axis=1)
-  #splitting the dataset
-  X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = 0.2, random_state = 42) 
-  # generate an xgboost regression model
-  model = XGBRegressor()
-  #fit the model
-  model.fit(X_train, y_train)
-  #predict for the test dataset
-  pred = model.predict(X_test)
-  _ = regression_report(test_set = X_test, target = y_test, predictions = pred )
-
-  """
-  df = pd.DataFrame(columns = ["Metric", "Acronym", "Value"])
-  metric_list = ["Mean Square Error", "Root Mean Square Error", "Max Error", "Mean Absolute Error", "Mean squared logarithmic error",
-                 "Mean Absolute Percentage Error", "R squared", "adjusted R squared", "Normalized RMSE (range)", "Normalized RMSE (quartile)"]
-  acron_list  = ["MAE", "RMSE", "max_err", "MAE", "MSLE", "MAPE", "R2", "adj_R2", "NRMSE", "NRMSE"]
-  df["Metric"], df["Acronym"]  = metric_list, acron_list
-  df.iloc[0,2] = MSE(target, predictions)
-  df.iloc[1,2] = RMSE(target, predictions)
-  df.iloc[2,2] = Max_error(target, predictions)
-  df.iloc[3,2] = MAE(target, predictions)
-  df.iloc[4,2] = MSLE(target, predictions)
-  df.iloc[5,2] = MAPE(target, predictions)
-  df.iloc[6,2] = R_squared(target, predictions)
-  df.iloc[7,2] = adj_R_squared(target, predictions,  n = len(target), k = test_set.shape[1] )
-  df.iloc[8,2] = NRMSE(target, predictions)
-  df.iloc[9,2] = NRMSE(target, predictions, norm_method = "quartile")
-  df1 = df.copy()
-  df1.value = df1.Value.astype("float32").round(decimals = 3)
-  print(df1)
-  return df
-
-k = regression_report(test_set = X_test, target = y_test, predictions = pred )
-
-import xgboost
-from xgboost import XGBRegressor
-from sklearn.model_selection import train_test_split
-import pandas as pd
-#boston dataset
-data_dir = "https://raw.githubusercontent.com/SalvatoreRa/tutorial/main/datasets/Boston.csv"
-df = pd.read_csv(data_dir)
-#separing the input features from the target variable
-y = df["medv"]
-X = df.drop(["medv"], axis=1)
-#splitting the dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = 0.2, random_state = 42) 
-# generate an xgboost regression model
-model = XGBRegressor()
-#fit the model
-model.fit(X_train, y_train)
-#predict for the test dataset
-pred = model.predict(X_test)
-
-import xgboost
-from xgboost import XGBRegressor
-from sklearn.model_selection import train_test_split
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.metrics import (explained_variance_score, max_error, mean_absolute_error, 
-                             mean_squared_error, mean_squared_log_error, median_absolute_error,
-                            r2_score,mean_absolute_percentage_error)
-data_dir = "https://raw.githubusercontent.com/SalvatoreRa/tutorial/main/datasets/Boston.csv"
-df = pd.read_csv(data_dir)
-df.describe() 
-#separing the input features from the target variable
-y = df["medv"]
-X = df.drop(["medv"], axis=1)
-#splitting the dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                        test_size = 0.2, #test size would be 20%
-                                                        random_state = 42 #42 is for good luck!
-                                                        ) 
-
-# generate an xgboost regression model
-model = XGBRegressor()
-#fit the model
-model.fit(X_train, y_train)
-#predict for the test dataset
-pred = model.predict(X_test)
-
-regression_report(test_set = X_test, target = y_test, predictions = pred )
 
