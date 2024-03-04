fb4361345a5aa4388eacdd654652a0aae9707992
Salvatore Raieli <32837577+SalvatoreRa@users.noreply.github.com>
2024-03-04 12:21:26 +0100

    Update utils_NA.py

--- machine learning/utility/utils_NA.py
+++ machine learning/utility/utils_NA.py
@@ -473,3 +473,32 @@ def produce_NA(X, p_miss, mecha="MCAR", opt=None, p_obs=None, q=None):
     X_nas[mask.bool()] = np.nan
     
     return {'X_init': X.double(), 'X_incomp': X_nas.double(), 'mask': mask}
+
+
+def missing_values_table(df):
+        # credit: https://www.kaggle.com/willkoehrsen/start-here-a-gentle-introduction. 
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
